fc54e3a0f90f04069b27219fb84fa35653af9f97
Varchini <varchinivc@gmail.com>
2026-03-04 18:16:29 +0530

    Update and rename install_httpd.sh to install_apache2.sh

--- scripts/install_apache2.sh
+++ scripts/install_apache2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-yum install httpd wget unzip -y
-systemctl start httpd
-systemctl enable httpd
+apt install apache2 wget unzip -y
+systemctl start apache2
+systemctl enable apache2
 
 cd /tmp/
 wget https://www.tooplate.com/zip-templates/2156_graphite_creative.zip
 unzip 2156_graphite_creative.zip
 cp -r 2156_graphite_creative/* /var/www/html/
 
-systemctl restart httpd
+systemctl restart apache2
