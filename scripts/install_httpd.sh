640e8797b947037f5e2e834d0db0f853cce8b22c
Varchini <varchinivc@gmail.com>
2026-03-04 18:07:21 +0530

    Update install_httpd.sh

--- scripts/install_httpd.sh
+++ scripts/install_httpd.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-yum install httpd -y
+yum install httpd wget unzip -y
 systemctl start httpd
 systemctl enable httpd
+
+cd /tmp/
+wget https://www.tooplate.com/zip-templates/2156_graphite_creative.zip
+unzip 2156_graphite_creative.zip
+cp -r 2156_graphite_creative/* /var/www/html/
+
+systemctl restart httpd
